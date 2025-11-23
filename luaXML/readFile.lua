3aabe71104c890518b50115bc7dda5d8d704d793
Davi <69681778+pessoa736@users.noreply.github.com>
2025-11-23 19:59:01 -0300

    Fix: readFile.
    por algum motivo o readFile tinha bugado

--- luaXML/readFile.lua
+++ luaXML/readFile.lua
@@ -3,4 +3,8 @@ return function (dirFile)
     local file <close> = io.open(dirFile, "r+")
     if not file then
         error("não foi possivel abrir o arquivo: ".. dirFile)
-    end                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+    end
+
+    local content = file:read("a")
+    return content
+end
